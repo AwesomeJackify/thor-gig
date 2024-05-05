3162e6b2d391608ff1f827f8580a50dcb0a2fdde
Jack Gong <jackgong48@gmail.com>
2024-05-05 21:34:51 +1200

    FIXED: mouse leave not working using overflow-hidden

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import image1 from "../assets/images/hero.jpg";
 
@@ -11,86 +11,75 @@ gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(TextPlugin);
 
 const Carousel = () => {
+  const [showCursor, setShowCursor] = useState(false);
+
   const { contextSafe } = useGSAP();
 
   let wrapperRef = useRef<HTMLDivElement | null>(null);
   let cursorRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    if (wrapperRef.current) {
-      wrapperRef.current.addEventListener(
-        "mouseleave",
-        contextSafe(() => {
-          if (cursorRef.current) {
-            if (wrapperRef.current) {
-              wrapperRef.current.style.cursor = "auto";
-            }
-            gsap.to(cursorRef.current, {
-              duration: 0.3,
-              scale: 0,
-              opacity: 0,
-            });
-          }
-        })
-      );
+  const handleMouseMove = contextSafe((e: any) => {
+    if (e.target && cursorRef.current && wrapperRef.current) {
+      var bounds = wrapperRef.current.getBoundingClientRect();
+      let x = e.clientX - 50;
+      let y = e.clientY - bounds.top - 50;
 
-      wrapperRef.current?.addEventListener(
-        "mouseenter",
-        contextSafe((e) => {
-          if (wrapperRef.current) {
-            wrapperRef.current.style.cursor = "none";
-          }
-          if (cursorRef.current) {
-            gsap.to(cursorRef.current, {
-              duration: 0.3,
-              scale: 1,
-              opacity: 1,
-            });
-          }
-        })
-      );
+      if (e.clientX < bounds.right / 2) {
+        gsap.to("#cursorText", {
+          duration: 0,
+          text: "Prev",
+        });
+        gsap.to("#thorIcon", {
+          duration: 0.3,
+          scaleX: -1,
+        });
+      } else {
+        gsap.to("#cursorText", {
+          duration: 0,
+          text: "Next",
+        });
+        gsap.to("#thorIcon", {
+          duration: 0.3,
+          scaleX: 1,
+        });
+      }
 
-      wrapperRef.current?.addEventListener(
-        "mousemove",
-        contextSafe((e) => {
-          if (e.target && cursorRef.current && wrapperRef.current) {
-            var bounds = wrapperRef.current.getBoundingClientRect();
-            let x = e.x - 50;
-            let y = e.y - bounds.top - 50;
-
-            if (e.x < bounds.right / 2) {
-              gsap.to("#cursorText", {
-                duration: 0,
-                text: "Prev",
-              });
-              gsap.to("#thorIcon", {
-                duration: 0.3,
-                scaleX: -1,
-              });
-            } else {
-              gsap.to("#cursorText", {
-                duration: 0,
-                text: "Next",
-              });
-              gsap.to("#thorIcon", {
-                duration: 0.3,
-                scaleX: 1,
-              });
-            }
-
-            gsap.to(cursorRef.current, {
-              duration: 0,
-              x: x,
-              y: y,
-            });
-          }
-        })
-      );
+      gsap.to(cursorRef.current, {
+        duration: 0,
+        x: x,
+        y: y,
+      });
     }
-  }, []);
+  });
+
+  const handleMouseEnter = contextSafe(() => {
+    if (cursorRef.current) {
+      gsap.to(cursorRef.current, {
+        duration: 0.3,
+        scale: 1,
+        opacity: 1,
+      });
+    }
+  });
+
+  const handleMouseLeave = contextSafe(() => {
+    if (cursorRef.current) {
+      gsap.to(cursorRef.current, {
+        duration: 0.3,
+        scale: 0,
+        opacity: 0,
+      });
+    }
+  });
 
   return (
-    <div ref={wrapperRef} className="w-full h-[500px] relative">
+    <div
+      ref={wrapperRef}
+      className="w-full h-[500px] overflow-hidden relative"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div
         ref={cursorRef}
         className="w-32 scale-0 opacity-0 aspect-square relative top-0 left-0 z-10 bg-secondary rounded-full flex justify-center items-center"
