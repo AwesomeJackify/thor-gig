e11c11bfc8e37b1b8e234e3dd81899e03e904747
Jack Gong <jackgong48@gmail.com>
2024-05-08 09:58:45 +1200

    ADDED: tilt to cursor

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -32,11 +32,14 @@ const Carousel = () => {
       let x = e.clientX - 50;
       let y = e.clientY - bounds.top - 50;
 
+      const centerX = window.innerWidth / 2;
+      const relativeX = (e.clientX - centerX) / window.innerWidth;
+
       if (e.clientX < bounds.right / 2) {
         setIsLeft(true);
         gsap.to("#cursorText", {
           duration: 0,
-          text: "Prev",
+          text: "← Prev",
         });
         gsap.to("#thorIcon", {
           duration: 0.3,
@@ -46,7 +49,7 @@ const Carousel = () => {
         setIsLeft(false);
         gsap.to("#cursorText", {
           duration: 0,
-          text: "Next",
+          text: "Next →",
         });
         gsap.to("#thorIcon", {
           duration: 0.3,
@@ -58,6 +61,11 @@ const Carousel = () => {
         duration: 0,
         x: x,
         y: y,
+      });
+
+      gsap.to(cursorRef.current, {
+        rotateZ: relativeX * 70,
+        duration: 0.3,
       });
     }
   });
@@ -118,20 +126,20 @@ const Carousel = () => {
     >
       <div
         ref={cursorRef}
-        className="w-32 scale-0 opacity-0 aspect-square relative top-0 left-0 z-50 bg-secondary rounded-full flex justify-center items-center pointer-events-none"
+        className="w-32 scale-0 opacity-0 aspect-square relative top-0 left-0 z-50 bg-secondary rounded-full flex justify-center items-center"
       >
         <Icon
           icon="game-icons:thor-hammer"
           id="thorIcon"
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -z-10 -translate-y-1/2 w-[80%] h-[80%] opacity-40"
         />
-        <h1 id="cursorText" className="uppercase text-white"></h1>
+        <h1 id="cursorText" className="uppercase text-white select-none"></h1>
       </div>
       <img
         id="mainImg"
         src={mainImage}
         alt="Carousel"
-        className="w-full h-full object-cover absolute top-0 left-0 z-10"
+        className="w-full h-full object-cover absolute top-0 left-0 z-10 select-none"
       />
       {/* <img
         id="secondImg"
